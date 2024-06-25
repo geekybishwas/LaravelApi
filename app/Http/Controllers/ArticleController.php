7e7c6d533d29d6354a257ad29664204cdc9363f5
geekybishwas <techiebishwas@gmail.com>
2024-06-25 17:27:53 +0545

    Feat:Joining 3 tables and accessing their fields

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -8,6 +8,15 @@ use Illuminate\Support\Facades\DB;
 class ArticleController extends Controller
 {
     public function index(){
-        DB::table('articles')->get();
+        $articles=DB::table('articles')
+        ->join('authors','articles.author_id','authors.id')
+        ->join('categories','articles.category_id','categories.id')
+        ->select('articles.*','authors.name','categories.cName')
+        ->get();
+
+        return response()->json([
+            'status'=>1,
+            'articles'=>$articles,
+        ],200);
     }
 }
